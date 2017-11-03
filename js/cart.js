b7b82c2032cd07da8396354a627c3fb22fa89e26
Kevin MIller <kevin@driftabout.com>
2017-11-03 03:03:29 -0700

    fixed bugs

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+var productKeys;
 var line_order;
 var order_image;
 var number_input;
@@ -20,8 +20,9 @@ function fillCart(){
   var products_quantities = order.products;
   var products = JSON.parse(localStorage.products);
   console.log(products)
-  for (var i = 0; i < products_quantities.length; i++){
-    imagePath = products[products_quantities[i].product].imagePath;
+  productKeys = Object.keys(products_quantities);
+  for (var i = 0; i < productKeys.length; i++){
+    imagePath = products[productKeys[i]].imagePath;
     line_order = document.createElement('li');
     order_image = document.createElement('img');
     order_image.setAttribute('src', imagePath);
@@ -31,12 +32,37 @@ function fillCart(){
     line_order.appendChild(title);
     number_input = document.createElement('input');
     number_input.setAttribute('type', 'number');
-    number_input.value = products_quantities[i].quantity;
+    number_input.value = products_quantities[productKeys[i]];
     line_order.appendChild(number_input);
     remove_button = document.createElement('button');
-    remove_button.setAttribute('type','button');
+    //remove_button.setAttribute('type','button');
+    remove_button.setAttribute('id',productKeys[i]);
     remove_button.textContent = 'remove from cart';
+    remove_button.addEventListener('click',  removeFromCart)
     line_order.appendChild(remove_button);
     selected_items_list.appendChild(line_order);
   }
+  addUserInfo()
+}
+
+function addUserInfo(){
+  var order = JSON.parse(localStorage.order);
+  var user_info_ids = ['name','address','city', 'state', 'zip', 'phone', 'credit_card'];
+  for (var i = 0; i < user_info_ids.length; i++){
+    console.log(user_info_ids[i]);
+    document.getElementById(user_info_ids[i]).textContent = order[user_info_ids[i]];
+  }
+}
+
+
+
+function  removeFromCart(){
+  var product_id = this.getAttribute('id')
+  var order = JSON.parse(localStorage.order);
+  delete order.products[product_id];
+  localStorage.order = JSON.stringify(order);
+  this.parentNode.style.display = 'none';
+  console.log('id: ', this.getAttribute('id'));
+  console.log('parent', this.parentNode);
+
 }
